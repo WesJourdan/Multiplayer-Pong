7f4aefb287e9d3c575f150523929465624610287
Wes Jourdan <wes.jourdan@icloud.com>
2018-03-25 00:38:04 -0400

    Nothing to see here.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PlayerPaddle from './bodies/paddle'
+import Ball from './bodies/ball'
+const Loop = require('./scripts/loop');
+
+let ballState = {
+  x: 1,
+  y: 5,
+  vx: 10,
+  vy: 10
+}
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
+    const style = {
+      width: '25em',
+      height: '18em',
+      backgroundColor: 'black'
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={style}>
+        < PlayerPaddle />
+        < Ball ball={ballState} />
       </div>
     );
+  }
+
+  componentDidMount () {
+    
+    Loop((tick) => {
+      ballState.x += ballState.vx * tick
+      ballState.y += ballState.vy * tick
+      this.forceUpdate()
+
+      // reverse velocity on collision with sides.
+      if (ballState.x > 23.9 || ballState.x < 0.1 ) {
+        ballState.vx *= -1
+      }
+      if (ballState.y > 16.9 || ballState.y < 0.1) {
+        ballState.vy *= -1
+      }
+
+    });
   }
 }
 
