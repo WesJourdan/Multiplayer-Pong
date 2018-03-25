906f36bffd9b681f10b0c9b4ae1b95bdd4d07c33
Wes Jourdan <wes.jourdan@icloud.com>
2018-03-25 15:50:23 -0400

    basic pong game functional.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import PlayerPaddle from './bodies/paddle'
-import Ball from './bodies/ball'
+import Paddle from './components/paddle'
+import Ball from './components/ball'
+import Scoreboard from './components/scoreboard'
 const Loop = require('./scripts/loop');
 
-let ballState = {
-  x: 1,
-  y: 5,
-  vx: 10,
-  vy: 10
+let gameState = {
+  ball: {
+    x: Math.floor(Math.random() * 10) + 5,
+    y: Math.floor(Math.random() * 10) + 5,
+    vx: 10 * (Math.random() < 0.5 ? 1 : -1),
+    vy: 10 * (Math.random() < 0.5 ? 1 : -1)
+  },
+  score: {
+    left: 0,
+    right: 0
+  },
+  paddle: {
+    left: {
+      x: 0.25,
+      y: 5
+    },
+    right: {
+      x: 29 - 0.25,
+      y: 5
+    }
+  }
+  
 }
 
 
@@ -20,15 +38,18 @@ class App extends Component {
 
   render() {
     const style = {
-      width: '25em',
-      height: '18em',
+      width: '30em',
+      height: '20em',
       backgroundColor: 'black'
     }
 
     return (
       <div style={style}>
-        < PlayerPaddle />
-        < Ball ball={ballState} />
+        < Scoreboard position={'left'} score={gameState.score.left} />
+        < Scoreboard position={'right'} score={gameState.score.right} />
+        < Paddle paddle={gameState.paddle.left} />
+        < Paddle paddle={gameState.paddle.right} />
+        < Ball ball={gameState.ball} />      
       </div>
     );
   }
@@ -36,16 +57,40 @@ class App extends Component {
   componentDidMount () {
     
     Loop((tick) => {
-      ballState.x += ballState.vx * tick
-      ballState.y += ballState.vy * tick
+      gameState.ball.x += gameState.ball.vx * tick
+      gameState.ball.y += gameState.ball.vy * tick
       this.forceUpdate()
 
       // reverse velocity on collision with sides.
-      if (ballState.x > 23.9 || ballState.x < 0.1 ) {
-        ballState.vx *= -1
+      if (gameState.ball.x > 28.9 && gameState.ball.vx > 0) {
+        gameState.ball.vx *= -1
+        gameState.score.left++
+        console.log(gameState.ball.y)
       }
-      if (ballState.y > 16.9 || ballState.y < 0.1) {
-        ballState.vy *= -1
+      if (gameState.ball.x < 0.1 && gameState.ball.vx < 0) {
+        gameState.ball.vx *= -1
+        gameState.score.right++
+        console.log(gameState.score)
+      }
+      if (gameState.ball.y > 19 && gameState.ball.vy > 0) {
+        gameState.ball.vy *= -1
+      }
+      if (gameState.ball.y < 0.1 && gameState.ball.vy < 0) {
+        gameState.ball.vy *= -1
+      }
+
+      //collision for paddles
+      if (gameState.ball.x > 28.9 - 1
+        && gameState.ball.vx > 0
+        && gameState.ball.y < gameState.paddle.right.y + 4
+        && gameState.ball.y >= gameState.paddle.right.y - 1) {
+        gameState.ball.vx *= -1
+      }
+      if (gameState.ball.x < 0.1 + 1
+        && gameState.ball.vx < 0
+        && gameState.ball.y < gameState.paddle.left.y + 4
+        && gameState.ball.y >= gameState.paddle.left.y - 1) {
+        gameState.ball.vx *= -1
       }
 
     });
